f063d786acb144bf48efbe95a4a6f53b9fdfaa92
Christopher Carroll Smith <chriscarrollsmith@gmail.com>
2024-03-18 12:13:06 -0400

    Fixed a few broken nav links

--- src/components/SimpleNav.tsx
+++ src/components/SimpleNav.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { HashLink } from 'react-router-hash-link';
-import { Link } from 'react-router-dom';
 import './SimpleNav.css';
 
 const FullNav: React.FC = () => {
@@ -8,23 +7,23 @@ const FullNav: React.FC = () => {
         <div className="mx-auto space-x-6">
             <div className="simplenavlinks">
                 <HashLink to="/#" className="simplenavlink">Home</HashLink>
-                <HashLink to="#contact" className="simplenavlink">
+                <HashLink to="/#contact" className="simplenavlink">
                     Contact
                 </HashLink>
-                <HashLink to="#story" className="simplenavlink">
+                <HashLink to="/#story" className="simplenavlink">
                     Story
                 </HashLink>
-                <HashLink to="#events" className="simplenavlink">
+                <HashLink to="/#events" className="simplenavlink">
                     Events
                 </HashLink>
-                <HashLink to="#credits" className="simplenavlink">
+                <HashLink to="/#credits" className="simplenavlink">
                     Credits
-                </HashLink>
-                <HashLink to="/Legal" className="simplenavlink">
-                    Privacy Policy
                 </HashLink>
                 <HashLink to="/Store" className="simplenavlink">
                     Store
+                </HashLink>
+                <HashLink to="/Legal" className="simplenavlink">
+                    Privacy Policy
                 </HashLink>
             </div>
         </div>
